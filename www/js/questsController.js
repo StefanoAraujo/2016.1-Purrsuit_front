5555f7cd79b38ac73c90e290c9a80f8f65ab0206
Tulio Coelho <tulioopc@gmail.com>
2016-05-28 11:33:09 -0300

    refactoring quests controller

--- www/js/questsController.js
+++ www/js/questsController.js
@@ -1,27 +1,29 @@
 angular.module('starter')
 
 // ALL -- Quests controller
-.controller('QuestsCtrl', function($scope, ServerQuests) {
-  ServerQuests.get(function(data) {
-      console.log("SERVICES: Getting Quests data from server...");
-      $scope.quests = data.quests;
+.controller('QuestsCtrl', function($scope, $stateParams, ServerQuests, ServerFindQuest) {
+
+  $scope.getQuests = function () {
+    ServerQuests.get(function(data) {
+        console.log("SERVICES: Getting Quests data from server...");
+        $scope.quests = data.quests;
+      },
+      function(error) {
+        alert("Não foi possível estabelecer conexão com o servidor...");
+        console.log("SERVICES: ERROR in getting Quests data from server...");
+      });
+  }
+  $scope.findQuest = function() {
+    var searchId = $stateParams.questId;
+
+    ServerFindQuest.get({id: searchId}, function(data){
+      console.log("SERVICES: Getting Quest (Id: " + searchId + ") data from server");
+      $scope.quest = data.quest;
     },
-    function(error) {
+    function(error){
       alert("Não foi possível estabelecer conexão com o servidor...");
-      console.log("SERVICES: ERROR in getting Quests data from server...");
-    });
-})
+      console.log("SERVICES: ERROR in getting Quest (Id: " + searchId + ") data from server");
+    })
+  }
 
-//SINGLE -- Quests Details controller
-.controller('QuestCtrl', function($scope, $stateParams, ServerFindQuest){
-  var searchId = $stateParams.questId;
-
-  ServerFindQuest.get({id: searchId}, function(data){
-    console.log("SERVICES: Getting Quest (Id: " + searchId + ") data from server");
-    $scope.quest = data.quest;
-  },
-  function(error){
-    alert("Não foi possível estabelecer conexão com o servidor...");
-    console.log("SERVICES: ERROR in getting Quest (Id: " + searchId + ") data from server");
-  })
 })
