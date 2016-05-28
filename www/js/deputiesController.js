fd4b697315ae01779950393e2eb435e81996560f
Tulio Coelho <tulioopc@gmail.com>
2016-05-28 10:47:28 -0300

    creating getDeputies function in controller scope

--- www/js/deputiesController.js
+++ www/js/deputiesController.js
@@ -1,7 +1,9 @@
 angular.module('starter')
 
-.controller('DeputiesCtrl', function($scope, ServerDeputies) {
-  ServerDeputies.get(function(data) {
+.controller('DeputiesCtrl',function($scope, ServerDeputies) {
+
+  $scope.getDeputies = function() {
+    ServerDeputies.get(function(data) {
       console.log("SERVICES: Getting Deputies data from server...")
       $scope.deputies = data.deputies;
     },
@@ -9,6 +11,14 @@ angular.module('starter')
       alert("Não foi possível estabelecer conexão com o servidor...");
       console.log("SERVICES: ERROR in getting Deputies data from server...");
     })
+  }
+  
+
+
+
+
+
+
 })
 
 .controller('DeputyCtrl', function($scope, $stateParams, ServerFindDeputy) {
