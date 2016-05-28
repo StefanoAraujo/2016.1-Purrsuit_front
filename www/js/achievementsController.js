884068406e4dacf951b782fdf4336d8f8e5bcec1
Tulio Coelho <tulioopc@gmail.com>
2016-05-28 11:32:18 -0300

    refactoring achivements controller

--- www/js/achievementsController.js
+++ www/js/achievementsController.js
@@ -1,26 +1,28 @@
 angular.module('starter')
 
-.controller('AchievementsCtrl', function($scope, ServerAchievements){
-  ServerAchievements.get(function(data){
-    console.log("SERVICES: Getting achievements data from server...");
-    $scope.achievements = data.achievements;
-  },
-  function(error){
-    alert("Não foi possível estabelecer conexão com o servidor...");
-    console.log("SERVICES: ERROR in getting Achievements data from server...");
-  })
-})
+.controller('AchievementsCtrl', function($scope, $stateParams, ServerAchievements, ServerFindAchv){
 
-// 'ACHV' means 'achievement'
-.controller('SingleACHV', function($scope, $stateParams, ServerFindAchv){
-  var searchId = $stateParams.achvId;
+  $scope.getAchievements =  function () {
+    ServerAchievements.get(function(data){
+      console.log("SERVICES: Getting achievements data from server...");
+      $scope.achievements = data.achievements;
+    },
+    function(error){
+      alert("Não foi possível estabelecer conexão com o servidor...");
+      console.log("SERVICES: ERROR in getting Achievements data from server...");
+    })
+  }
 
-  ServerFindAchv.get({id: searchId}, function(data){
-    console.log("SERVICES: Getting Achievement (Id: " + searchId + ") data from server...");
-    $scope.achievement = data.achievement;
-  },
-  function(error){
-    alert("Não foi possível estabelecer conexão com o servidor...");
-    console.log("SERVICES: ERROR in getting Achievement (Id: " + searchId + ") data from server...");
-  })
+  $scope.singleAchievement =  function () {
+    var searchId = $stateParams.achvId;
+
+    ServerFindAchv.get({id: searchId}, function(data){
+      console.log("SERVICES: Getting Achievement (Id: " + searchId + ") data from server...");
+      $scope.achievement = data.achievement;
+    },
+    function(error){
+      alert("Não foi possível estabelecer conexão com o servidor...");
+      console.log("SERVICES: ERROR in getting Achievement (Id: " + searchId + ") data from server...");
+    })
+  }
 })
